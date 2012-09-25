dbdf77bedfb9fda13b3860795e18c135e3ab4e58
suin <suinyeze@gmail.com>
2012-09-26 01:54:00 +0900

    一覧に表示するユーザをグループ指定できるようにした

--- html/modules/xoopsmembers/index.php
+++ html/modules/xoopsmembers/index.php
@@ -4,12 +4,23 @@ require dirname(__FILE__).'/../../mainfile.php';
 
 $criteria = new CriteriaCompo();
 $criteria->add(new Criteria('level', 0, '>'));
+
+// We can not use IN statement here,
+// because XoopsObjectGenericHandler doesn't support a conditon which is not a property of an XoopsObject
+$subcriteria = new CriteriaCompo();
+
+foreach ( $xoopsModuleConfig['listed_groups'] as $groupId )
+{
+	$subcriteria->add(new Criteria('g.groupid', $groupId), 'OR');
+}
+
+$criteria->add($subcriteria);
 $criteria->addSort('uname', 'ASC');
 
-/** @var $memberHandler XoopsMemberHandler */
-$memberHandler = xoops_gethandler('member');
-$xoopsTpl->assign('total', $memberHandler->getUserCount($criteria));
-$xoopsTpl->assign('users', $memberHandler->getUsers($criteria, true));
+/** @var $userGroupHandler Xoopsmembers_UserGroupHandler */
+$userGroupHandler = xoops_getmodulehandler('UserGroup');
+$xoopsTpl->assign('total', $userGroupHandler->getCount($criteria));
+$xoopsTpl->assign('users', $userGroupHandler->getObjects($criteria));
 
 require_once XOOPS_ROOT_PATH."/header.php";
 $xoopsOption['template_main'] = 'xoopsmembers_searchresults.html';
